ca042442affe6c8b418e73057b7fe737b51c1d21
Mieszko135 <mieszko-braun@wp.pl>
2022-06-07 20:19:26 +0200

    footer

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Carousel } from './elements/Carousel';
 import { Header } from './elements/Header';
 import { CarouselData } from './elements/CarouselData';
 import { TopRatedPhotos } from "./elements/TopRatedPhotos"
+import { Footer } from './elements/Footer';
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <Header />
       <Carousel slides={CarouselData}/>
       <TopRatedPhotos />
+      <Footer />
     </>
   )
 }
---
+++ src/elements/Footer.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export function Footer() {
+    return (
+        <footer className="page-footer">
+            <div className="page-footer__container container">
+                Copyright &copy; Mieszko Braun Fotografia
+            </div>
+        </footer>
+    )
+}
