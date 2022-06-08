f98db72c0dbf9fe79d14326f339d5330ff19622d
Mieszko135 <mieszko-braun@wp.pl>
2022-06-08 22:04:50 +0200

    about me

---
+++ src/elements/AboutMe.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export function AboutMe() {
+    return (
+        <div className="about-me-container container">
+            <h2 className="about-me__header">O mnie</h2>
+            <p className="about-me__text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas commodo euismod sodales. Morbi et magna a diam viverra tristique quis bibendum ipsum. Mauris blandit nisl vel cursus tincidunt. Curabitur vulputate ultricies orci, id fringilla mi pharetra a. Sed facilisis tortor vitae ultricies dignissim. Sed pulvinar massa nec mi consectetur interdum. In sit amet dapibus dolor, vitae volutpat leo.</p>
+        </div>
+    )
+}
--- src/elements/Main.js
+++ src/elements/Main.js
@@ -1,14 +1,17 @@
+import React from 'react';
 import { Header } from './Header';
 import { Carousel } from './Carousel';
 import { CarouselData } from './CarouselData';
 import { TopRatedPhotos } from "./TopRatedPhotos"
 import { Footer } from './Footer';
+import { AboutMe } from './AboutMe';
 
 export function Main() {
   return (
     <>
       <Header />
       <Carousel slides={CarouselData}/>
+      <AboutMe />
       <TopRatedPhotos />
       <Footer />
     </>
